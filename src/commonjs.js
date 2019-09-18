34d036a51d5414dd61c9930c9843ad2812f3e4e6
vandeurenglenn <vandeurenglenn@gmail.com>
2019-09-18 21:21:48 +0200

    :+1:  Add successOrFail to commonjs

--- src/commonjs.js
+++ src/commonjs.js
@@ -3,22 +3,37 @@ import { join } from 'path';
 import { homedir } from 'os';
 
 export default class LeofcoinStorage {
+  
   constructor(path) {
     if (!path) path = join(homedir(), '.leofcoin')
     this.path = path;
   }
   
+  async _successOrFail(fn) {
+    let result;
+    try {
+      result = await fn;
+    } catch (e) {
+      result = undefined;
+    }
+    return result;
+  }
+  
   async get(path, encoding = 'json') {
-    return await read(join(this.path, path), encoding)
+    return await _successOrFail(read(join(this.path, path), encoding));
   }
   
   async set(path, value) {
-    if (value instanceof Object) value = JSON.stringify(value);
-    
-    return await write(join(this.path, path), value);    
+    if (value instanceof Object) value = JSON.stringify(value);      
+    return await _successOrFail(write(join(this.path, path), value););
   }
   
   async delete() {
-    return await unlink(join(this.path, path))
+    try {
+      await unlink(join(this.path, path))
+    } catch (e) {
+      console.warn(e);
+    }
+    return;
   }
 };
