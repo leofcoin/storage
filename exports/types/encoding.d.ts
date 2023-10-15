f594ae15b72e57b2b51203cd44c6bf6ba1515f40
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-10-15 12:31:16 +0200

    Cleanup exports & update ts config

--- exports/types/encoding.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const encode: (string: string) => Uint8Array;
-export declare const decode: (uint8Array: Uint8Array) => string;
--- exports/types/value.d.ts
+++
@@ -1,6 +0,0 @@
-export default class KeyValue {
-    uint8Array: Uint8Array;
-    constructor(input: string | Uint8Array | KeyValue);
-    isKeyValue(): boolean;
-    toString(): string;
-}
